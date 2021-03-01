9fd380676dae9d2c5a16ba69c72e7eb74479f268
xiaoxue123123 <xiaoxuez2016@gmail.com>
2021-03-01 17:42:38 -0600

    largestrectanglearea

---
+++ Practices/src/Stack/LargestRectagleArea.java
@@ -0,0 +1,28 @@
+package Stack;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class LargestRectagleArea {
+    public int largestRectangleArea(int[] heights) {
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.offerFirst(-1);
+        int length = heights.length;
+        int maxArea = 0;
+        for (int i = 0; i < length; i++) {
+            while ((stack.peek() != -1) && (heights[stack.peek()] >= heights[i])) {
+                int currentHeight = heights[stack.pop()];
+                int currentWidth = i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+            }
+            stack.push(i);
+        }
+
+        while (stack.peek() != -1) {
+            int currentHeight = heights[stack.pop()];
+            int currentWidth = length - stack.peek() - 1;
+            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+        }
+        return maxArea;
+    }
+}
